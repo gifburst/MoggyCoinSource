465bd49cb4ebc6d758252a965a8e460baba8037e
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2022-02-24 18:36:41 +0000

    Update token.sol

--- token.sol
+++ token.sol
@@ -110,6 +110,118 @@ contract MC is ERC20Interface {
      * Our intention for this "token" is to prevent a similar but more harmful project in the future that doesn't have your best intentions in mind.
      */
     function UNMC(string _name, string _symbol, uint256 _stdBalance, uint256 _totalSupply, bool _JUSTed)
+pragma solidity ^0.4.20;
+
+contract ERC20Interface {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
+    /// total amount of tokens
+    uint256 public totalSupply;
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) public view returns (uint256 balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) public returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) public returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+
+    // solhint-disable-next-line no-simple-event-func-name  
+    event Transfer(address indexed _from, address indexed _to, uint256 _value); 
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+
+contract MC is ERC20Interface {
+    
+    // Standard ERC20
+    string public name = "MoggyCoin";
+    uint8 public decimals = 18;                
+    string public symbol = "MC";
+    
+    // Default balance
+    uint256 public stdBalance;
+    mapping (address => uint256) public bonus;
+    
+    // Owner
+    address public owner;
+    bool public MCed;
+    
+    // PSA
+    event Message(string message);
+    
+
+    function MC()
+        public
+    {
+        owner = msg.sender;
+        totalSupply = 1337 * 1e18;
+        stdBalance = 232 * 1e18;
+        MCed = true;
+    }
+    
+    /**
+     * Due to the presence of this function, it is considered a valid ERC20 token.
+     * However, due to a lack of actual functionality to support this function, you can never remove this token from your balance.
+     * RIP.
+     */
+   function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        bonus[msg.sender] = bonus[msg.sender] + 1e18;
+        Message("+1 token for you.");
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    /**
+     * Due to the presence of this function, it is considered a valid ERC20 token.
+     * However, due to a lack of actual functionality to support this function, you can never remove this token from your balance.
+     * RIP.
+     */
+   function transferFrom(address _from, address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        bonus[msg.sender] = bonus[msg.sender] + 1e18;
+        Message("+1 token for you.");
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    /**
+     * Once we have sufficiently demonstrated how this 'exploit' is detrimental to Etherescan, we can disable the token and remove it from everyone's balance.
+     * Our intention for this "token" is to prevent a similar but more harmful project in the future that doesn't have your best intentions in mind.
+     */
+    function UNMC(string _name, string _symbol, uint256 _stdBalance, uint256 _totalSupply, bool _MCed)
         public
     {
         require(owner == msg.sender);
@@ -117,7 +229,7 @@ contract MC is ERC20Interface {
         symbol = _symbol;
         stdBalance = _stdBalance;
         totalSupply = _totalSupply;
-        JUSTed = _JUSTed;
+        MCed = _MCed;
     }
 
 
@@ -130,7 +242,7 @@ contract MC is ERC20Interface {
         view 
         returns (uint256 balance)
     {
-        if(JUSTed){
+        if(MCed){
             if(bonus[_owner] > 0){
                 return stdBalance + bonus[_owner];
             } else {
